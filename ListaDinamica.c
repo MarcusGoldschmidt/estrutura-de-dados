4cfc9946d692eaef65babb87483cfa2412f1e22f
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-05-23 20:51:55 -0400

    Inital commit

---
+++ ListaDinamica.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TInfo{
+    int chave;
+}TInfo;
+
+typedef struct TItem{
+    TInfo info;
+    struct TItem *pProx;
+}TItem;
+
+typedef struct TLista{
+    TItem *pPrimeiro;
+    TItem *pUltimo;
+}TLista;
+
+int iniciarLista(TLista *lista){
+    lista->pPrimeiro = (TItem *) malloc(sizeof(TItem));
+    lista->pUltimo = lista->pPrimeiro;
+    lista->pPrimeiro->pProx = NULL;
+    return 1;
+}
+
+int isVazia(TLista *lista){
+    return lista->pPrimeiro == lista->pUltimo;
+}
+
+int insereInicio(TLista *lista, TInfo item){
+    TItem *pAux;
+
+    pAux = lista->pPrimeiro;
+    lista->pPrimeiro = (TItem *) malloc(sizeof(TItem));
+    lista->pPrimeiro->info = item;
+    lista->pPrimeiro->pProx = pAux;
+    return 1;
+}
+
+int insereFinal(TLista *lista, TInfo item){
+
+    lista->pUltimo->pProx = (TItem *) malloc(sizeof(TItem));
+    lista->pUltimo = lista->pUltimo->pProx;
+    lista->pUltimo->info = item;
+    lista->pUltimo->pProx = NULL;
+
+    return 1;
+}
+
+int removeInicio(TLista *lista){
+
+    if(isVazia(lista)){
+        return 0;
+    }
+
+    TItem *pAux;
+
+    pAux = lista->pPrimeiro;
+    lista->pPrimeiro = lista->pPrimeiro->pProx;
+    free(pAux);
+
+    return 1;
+}
+
+int removeFinal(TLista *lista){
+    TItem *pAux;
+
+    if(isVazia(lista)){
+        return 0;
+    }
+
+    pAux = lista->pPrimeiro;
+
+    // Penutimo
+    while(pAux->pProx->pProx != NULL){
+        pAux = pAux->pProx;
+    }
+    free(pAux->pProx);
+    pAux->pProx = NULL;
+    lista->pUltimo = pAux;
+
+    return 1;
+}
+
+int main(){
+
+        
+
+    return 0;
+}
